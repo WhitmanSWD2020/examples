d3414a4ee901c69fb8ace075b34c05454ed2d72c
johnastratton <johnastratton@users.noreply.github.com>
2020-03-04 11:51:29 -0800

    Final in-class version

--- Dogs.java
+++ Dogs.java
@@ -18,11 +18,20 @@ class Dog {
       }
     }
   }
+  public void makeSound(Dog other) {
+    makeSound();
+  }
 }
 
 class Hound extends Dog {
   public Hound() {
     sound = "bay";
+  }
+  public void makeSound(Dog other) {
+    System.out.println("sniff");
+  }
+  public void makeSound(Hound other) {
+    System.out.println("whuff");
   }
 }
 
@@ -31,5 +40,14 @@ public class Dogs {
     Dog dog = new Dog();
     Dog doug = new Hound();
     Hound hound = new Hound();
+    
+    dog.makeSound(); //Step 1 -> makeSound(), Step 2: Dog.makeSound()
+    dog.makeSound(doug); //Step 1 -> makeSound(Dog), Step 2: Dog.makeSound(Dog)
+    hound.makeSound(doug); //Step 1 -> makeSound(Dog), Step 2: Hound.makeSound(Dog)
+    hound.makeSound(hound); //Step 1 -> makeSound(Hound), Step 2: Hound.makeSound(Hound)
+    dog.makeSound(hound); //Step 1 -> makeSound(Dog), Step 2: Dog.makeSound(Dog)
+    doug.makeSound(dog); //Step 1 -> makeSound(Dog), Step 2: Hound.makeSound(Dog)
+    doug.makeSound(hound); //Step 1 -> makeSound(Dog), Step 2: Hound.makeSound(Dog)
+    
   }
 }
